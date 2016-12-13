662a3c95ccf2798dc91e2f332a97b29eb379dcd8
ergl <ergl@users.noreply.github.com>
2016-12-13 16:06:52 +0100

    Check for remote node control file before stopping

--- script/g5k/control-nodes.sh
+++ script/g5k/control-nodes.sh
@@ -16,11 +16,10 @@ startNodes () {
 
 stopNodes () {
   local command="\
-    ./control-nodes-remote.sh stop; \
+    [[ -f control-nodes-remote.sh ]] && ./control-nodes-remote.sh stop; \
     pkill beam; \
     rm -rf antidote/_build/default/rel/antidote/data/*; \
     rm -rf antidote/_build/default/rel/antidote/log/*; \
-    ./control-nodes-remote.sh stop
   "
   ./execute-in-nodes.sh "$(cat ${ANT_NODES})" "${command}" "-debug"
 }
