9cbb396bcebb99947da9da22c085518b7c9e246a
ergl <ergl@users.noreply.github.com>
2016-12-13 14:13:12 +0100

    Add NTP sync for Antidote nodes

--- script/g5k/sync-time.sh
+++ script/g5k/sync-time.sh
@@ -1,26 +1,24 @@
 #!/usr/bin/env bash
 
-set -eo pipefail
-
-syncTime () {
-  $(while true; do
-    local command="\
-      service ntp stop && \
-      /usr/sbin/ntpdate -b ntp2.grid5000.fr && \
-      service ntp start
-    "
-    ./execute-in-nodes.sh "$(cat ${ANT_NODES})" "${command}" "-debug"
-    sleep 60
-  done) &
-
-  echo "${!}"
-}
-
-if [[ $# -eq 1 && "$1" == "--start" ]]; then
-  echo "$(syncTime)"
-elif [[ $# -eq 2 && "$1" == "--stop" ]]; then
-  kill $2
-else
-  echo "Usage: ${0##*/} [--start] [--stop pid]"
+if [[ $# -ne 1 ]]; then
+  echo "Usage: ${0##*/} --start | --stop"
   exit 1
 fi
+
+startSync () {
+  local antidote_nodes=( $(cat ${ANT_NODES}) )
+  for node in "${antidote_nodes[@]}"; do
+    scp -i ${EXPERIMENT_PRIVATE_KEY} ./sync-time-remote.sh root@${node}:/root/
+  done
+
+  ./execute-in-nodes.sh "$(cat ${ANT_NODES})" "./sync-time-remote.sh --start" "-debug"
+}
+
+stopSync () {
+  ./execute-in-nodes.sh "$(cat ${ANT_NODES})" "./sync-time-remote.sh --stop" "-debug"
+}
+
+case "$1" in
+  "--start") startSync;;
+  "*") stopSync
+esac
